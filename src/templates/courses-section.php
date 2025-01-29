b57916ed542490d6f3ea3fea4e145c7f0acee526
jguerreiro <joel.borghi@gmail.com>
2025-01-29 19:34:38 -0300

    Couple CSS fixes

--- src/templates/courses-section.php
+++ src/templates/courses-section.php
@@ -12,8 +12,15 @@
         <?php require_once __DIR__ . '/../views/course-list.php'; ?>
         <div class="course-card">
             <a href="#">
-                <img class="card-img" src="../../images/add-course.png"></img>
+                <img class="card-img add-course" src="../../images/add-course.jpg"></img>
             </a>
+            <div class="card-inner">
+                <h2>Add course to your collection</h2>
+                <p>Here you can add more courses!</p>
+                <div class="course-cta">
+                    <a href="#">ADD COURSE</a>
+                </div>
+            </div>
         </div>
     </div>
 </section>
